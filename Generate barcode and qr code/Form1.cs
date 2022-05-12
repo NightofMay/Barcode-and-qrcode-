5a0fa8c3b29dc1144cb1199bfa9097ea625211fa
Leila May <102301669+NightofMay@users.noreply.github.com>
2022-05-12 14:44:10 +0800

    Add files via upload

---
+++ Generate barcode and qr code/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Generate_barcode_and_qr_code
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Zen.Barcode.Code25BarcodeDraw barcode = Zen.Barcode.BarcodeDrawFactory.Code25StandardWithChecksum;
+            pictureBox1.Image = barcode.Draw(textBox1.Text, 50); 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Zen.Barcode.CodeQrBarcodeDraw qrcode = Zen.Barcode.BarcodeDrawFactory.CodeQr;
+            pictureBox1.Image = qrcode.Draw(textBox1.Text, 50); 
+        }
+    }
+}
